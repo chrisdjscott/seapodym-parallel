73032790d12f935267b506ceac8892fb29c3df60
Alexander Pletzer <alexander@gokliya.net>
2025-05-21 10:02:17 +1200

    Adding some files

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # seapodym-parallel
 Code development of population dynamics models aka SEAPODYM with parallel runs and parameter estimations
+
+## Prerequisites
+
+You will need:
+ * A C++ compiler (e.g. g++) and MPI libraries installed (e.g. OpenMPI)
+ * CMake
+
+
+## How to build the seapodym-parallel
+
+```
+git clone  git@github.com:PacificCommunity/seapodym-parallel
+cd seapodym-parallel
+mkdir build
+cd build
+cmake ..
+```
+
+## How to build the documention
+
+In the build directory
+```
+cmake --build . --target doc_doxygen
+```
+This will generate the documention in the top source directory `docs`.
+
+## How to test the code
+
+In the build directory
+```
+ctest
+```
---
+++ src/spcohort/SeapodymCohort.h
@@ -0,0 +1,56 @@
+#include <string>
+
+#ifndef SEAPODYM_COHORT
+#define SEAPODYM_COHORT
+
+class SeapodymCohort {
+
+    /**
+     * @brief Constructor
+     * @param parFile XML input file
+     * @param id unique identifier of this cohort (e.g. based on the date yyyymmdd of birth)
+     */
+    SeapodymCohort(const std::string& parFile, int id);
+
+    /**
+     * @brief Constructor
+     * @param restartFile Restaret file
+     */
+
+    SeapodymCohort(const std::string& restartFile);
+
+    /**
+     * Constructor from other cohorts' spawning data
+     * @param data serialized data
+     */
+    SeapodymCohort(const double* data);
+
+
+    /**
+     * @brief Step forward
+     * @param paramVector
+     */
+     void stepForward(const dvar_vector& paramVector);
+
+     /**
+      * Set state from array
+      * @param array serialization of the object's state
+      */
+    void setStateFromArray(const double* array);
+  
+
+     /**
+      * @brief Serialize the state
+      * @return array serialization of the object's state
+      */
+    double* getArrayFromState() const;
+
+     /**
+      * @brief Save the current state to a file
+      * @param restartFile
+      */
+    void save(const std::string& restartFile) const;
+
+};
+
+#endif // SEAPODYM_COHORT
