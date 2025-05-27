5ec128411c90f74185300f7593c5a1d8f5d7e05e
Alexander Pletzer <alexander@gokliya.net>
2025-05-28 11:54:09 +1200

    + test

---
+++ tests/testSeapodymTaskManager.cxx
@@ -0,0 +1,14 @@
+#include <SeapodymTaskManager.h>
+
+void test1() {
+    int na = 2;
+    int nw = 2;
+    int nt = 3;
+    SeapodymTaskManager tm(na, nw, nt);
+
+}
+
+int main() {
+    test1();
+    return 0;
+}
