45f398d2bce14602ff236b041ee971479cf1bd3d
Alexander Pletzer <alexander@gokliya.net>
2025-05-28 16:03:18 +1200

    fixed doxygen, fixed test

--- src/SeapodymTaskManager.h
+++ src/SeapodymTaskManager.h
@@ -5,6 +5,12 @@
 #ifndef SEAPODYM_TASK_MANAGER
 #define SEAPODYM_TASK_MANAGER
 
+/**
+ * Class SeapodymTaskManager
+ * @brief The SeapodymTaskManager knows how to distribute tasks across workers, how many times a tasks needs to be executed 
+ *        and what the next task should be given to a worker.
+ */
+
 class SeapodymTaskManager {
 
     private:
@@ -15,11 +21,13 @@ class SeapodymTaskManager {
         // number of workers 0...numWorkers-1
         int numWorkers;
 
-        // number of time steps
+        // total number of time steps across all cohorts
         int numTimeSteps;
 
+        // number of cohorts
         int numCohorts = this->numAgeGroups + this->numTimeSteps - 1;
 
+        // assign tasks to workers
         std::map<int, std::vector<int> > worker2task;
 
     public:
@@ -47,8 +55,18 @@ class SeapodymTaskManager {
          */
         int getNumSteps(int taskId) const;
 
+        /**
+         * Get the dependencies of a new task on othe preceeding tasks
+         * @param taskId task ID
+         * @return all the other tasks that feed into this task
+         */
         std::set<int> getDependencies(int taskId) const;
 
+        /**
+         * Get the task that follows a terminated task
+         * @param taskId task ID
+         * @return the next task
+         */
         int getNextTask(int taskId) const;
 
 };
--- tests/testSeapodymTaskManager.cxx
+++ tests/testSeapodymTaskManager.cxx
@@ -1,29 +1,54 @@
 #include <iostream>
 #include <SeapodymTaskManager.h>
 
-void test1() {
-    int na = 3;
-    int nw = 3;
-    int nt = 5;
+void test(int na, int nw, int nt) {
+
     SeapodymTaskManager tm(na, nw, nt);
 
-    for (auto iw = 0; iw < na; ++iw) {
-        std::cout << "worker " << iw << ": "; 
+    // iterate over the workers
+    for (auto iw = 0; iw < nw; ++iw) {
+        std::cout << "test(" << na << ", " << nw << ", " << nt << ") worker " << iw << ": ";
+
+        // get the initial tasks for this worker
         auto tasks = tm.getInitTaskIds(iw);
-        for (auto tid : tasks) {
+
+        // itereate over the inital tasks
+        for (auto t : tasks) {
+
+            // set the task
+            int tid = t;
+
+            // step forward until the next task is < 0
             while (tid >= 0) {
+
+                // get the number of steps for this task
                 auto nsteps = tm.getNumSteps(tid);
+
+                // march forward nsteps
                 for (auto i = 0; i < nsteps; ++i) {
                     std::cout << tid << " ";
                 }
+
+                // are there more tasks to execute?
                 tid = tm.getNextTask(tid);
             }
+            std::cout << ", ";
         }
         std:: cout << '\n';
     }
+    std::cout << "=====================\n";
 }
 
 int main() {
-    test1();
+
+    // 3 age groups, 3 workers, 5 steps
+    test(3, 3, 5);
+
+    // 3 age groups, 3 workers, 10 steps
+    test(3, 3, 10);
+
+    // 3 age groups, 2 workers, 5 steps
+    test(3, 2, 5);
+    
     return 0;
 }
